b638e7aee403fc9845712707d03f07c8390bff9e
VasilyBrin <mail@profitdata.ru>
2021-10-20 19:27:45 +0300

    Routing. Creating the first route

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 
 const app = express(); // its working
+const products = ['Apple','Pen','Computer'];
 
 app.get('/', (req,res,next) => {
 	res.send('Its working');
+});
+
+app.get('/products', (req, res, next) => {
+	res.send(products);
+});
+
+app.get('/products', (req, res, next) => {
+	res.send(1);
 });
 
 app.listen(5000, () => {
