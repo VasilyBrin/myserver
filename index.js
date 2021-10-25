5f5f8011ac06583963d32951ee53f67dd85b6495
VasilyBrin <mail@profitdata.ru>
2021-10-25 22:51:34 +0300

    redirect

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const booksRouter = express.Router();
 
 const app = express(); // its working
 const products = ['Apple','Pen','Computer'];
+
+app.use('/static', express.static(__dirname + '/public'));
 
 app.get('/', (req,res,next) => {
 	res.send('Its working');
 });
 
 app.get('/products', (req, res, next) => {
-	console.log('Page', req.query.page);
-	res.send(products);
+  next();
+  // res.json({products});
 });
 
 app.get('/products/:id', (req, res, next) => {
@@ -21,15 +22,15 @@ app.get('/products/:id', (req, res, next) => {
 	}
 });
 
-booksRouter.get('/', (req,res) => {
-	res.send('Books');
-})
-
-booksRouter.get('/about', (req, res) => {
-  res.send('About books');
+app.get('/downloadBooks', (req, res, next) => {
+	res.dounload('./public/books.html', 'anothername', (err) => {
+		console.log('File sent');
+	});
 });
 
-app.use('/books', booksRouter);
+app.get('/blog', (req, res, next) => {
+  res.redirect(301,'/');
+});
 
 app.listen(5000, () => {
 	console.log('Its started', new Date());
